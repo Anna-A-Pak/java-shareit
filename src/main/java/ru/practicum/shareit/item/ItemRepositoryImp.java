a055f9af4ef6a5c333db25b9969dad0c59faf601
Anna-A-Pak <anna.pak.tech@gmail.com>
2026-08-23 12:48:45 +0300

    Исправить ошибку

--- src/main/java/ru/practicum/shareit/item/ItemRepositoryImp.java
+++ src/main/java/ru/practicum/shareit/item/ItemRepositoryImp.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.item;
 
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.user.User;
 
 import java.util.*;
